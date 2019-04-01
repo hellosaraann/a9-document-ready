589015e2aad9d7c5877ca929489fac556f7c180b
hellosaraann <srichardson@mcad.edu>
2019-03-31 19:43:37 -0500

    Beginning js and html added

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-var llamaList = new Vue ({
-  el: #myApp,
-  llamaFacts: {
-    items: [
+var llamaApp = new Vue ({
+  el: '#myApp',
+  data: {
+    message: 'Facts (and opinions) about llamas',
+    llamaFacts: {
       llamaBreed: 'Llamas are domesticated South American camelids.',
-      llamaDesign: 'Llamas appear to have been put together by different designers with odd parts and have funny, floppy hair on their heads.',
+      llamaDesign: 'Llamas appear to have been put together by different designers with odd parts.',
       llamaSize: 'The height of a full-grown, full-size llama is 5 ft 7 in to 5 ft 11 in tall at the top of the head, and they can weigh between 290 and 440 lb.',
       llamaGoogleSearch: 'The second most popular Google Search pertaining to llamas is \"llamas with hats\".',
       llamaDangerFactor: 'There is a thing called Berserk llama syndrome.',
       llamaSmell: 'For outdoor animals, \"...they do not smell bad,\".',
       llamasInUS: 'Llamas were first imported to the U.S. in the late 1800s to be shown off as \"oddities\" in zoos.',
       moodyLlamas: 'An unhappy llama will stick out its tongue or even spit at another llama.',
-      llamasHum: 'Llamas hum when they're anxious, tired or just curious, and mama llamas hum to baby llamas.
-      llamaOrgle: 'When a male is interested in a female, he'll often make a gargling noise, called an orgle.'
-    ]
-  }
+      llamasHum: 'Llamas hum when they\'re anxious, tired or just curious, and mama llamas hum to baby llamas.',
+      llamaOrgle: 'When a male is interested in a female, he\'ll often make a gargling noise, called an orgle.'
+      }
+    }
 });
