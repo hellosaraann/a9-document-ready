6156ab107e96284cebde57f72670494cfcf5b1fe
hellosaraann <srichardson@mcad.edu>
2019-03-31 22:18:10 -0500

    Added css and v-if

--- script.js
+++ script.js
@@ -1,18 +1,48 @@
 var llamaApp = new Vue ({
   el: '#myApp',
   data: {
-    message: 'Facts (and opinions) about llamas',
-    llamaFacts: {
-      llamaBreed: 'Llamas are domesticated South American camelids.',
-      llamaDesign: 'Llamas appear to have been put together by different designers with odd parts.',
-      llamaSize: 'The height of a full-grown, full-size llama is 5 ft 7 in to 5 ft 11 in tall at the top of the head, and they can weigh between 290 and 440 lb.',
-      llamaGoogleSearch: 'The second most popular Google Search pertaining to llamas is \"llamas with hats\".',
-      llamaDangerFactor: 'There is a thing called Berserk llama syndrome.',
-      llamaSmell: 'For outdoor animals, \"...they do not smell bad,\".',
-      llamasInUS: 'Llamas were first imported to the U.S. in the late 1800s to be shown off as \"oddities\" in zoos.',
-      moodyLlamas: 'An unhappy llama will stick out its tongue or even spit at another llama.',
-      llamasHum: 'Llamas hum when they\'re anxious, tired or just curious, and mama llamas hum to baby llamas.',
-      llamaOrgle: 'When a male is interested in a female, he\'ll often make a gargling noise, called an orgle.'
+    message: 'Any non-factual opinions of the author that accidentally made it to this list are hidden.',
+    llamaFacts: [
+      {
+        id: 'llamaBreed',
+        text: 'Llamas are domesticated South American camelids.'
+      },
+      {
+        id: 'llamaSize',
+        text: 'The height of a full-grown, full-size llama is 5 ft 7 in to 5 ft 11 in tall at the top of the head, and they can weigh between 290 and 440 lb.'
+      },
+      {
+        id: 'llamaGoogleSearch',
+        text: 'The second most popular Google Search pertaining to llamas is \"llamas with hats\".'
+      },
+      {
+        id: 'llamaDangerFactor',
+        text: 'There is a thing called Berserk Llama Syndrome, and it\'s not pretty.'
+      },
+      {
+        id: 'llamaSmell',
+        text: 'For outdoor animals, \"...they do not smell bad.\"'
+      },
+      {
+        id: 'llamaDesign',
+        text: 'Llamas appear to have been put together by different designers with odd parts.'
+      },
+      {
+        id: 'llamasInUS',
+        text: 'Llamas were first imported to the U.S. in the late 1800s to be shown off as \"oddities\" in zoos.'
+      },
+      {
+        id: 'moodyLlamas',
+        text: 'An unhappy llama will stick out its tongue or even spit at another llama.'
+      },
+      {
+        id: 'llamasHum',
+        text: 'Llamas hum when they\'re anxious, tired or just curious, and mama llamas hum to baby llamas.'
+      },
+      {
+        id: 'llamaOrgle',
+        text: 'When a male is interested in a female, he\'ll often make a gargling noise, called an orgle.'
       }
-    }
+    ]
+  }
 });
